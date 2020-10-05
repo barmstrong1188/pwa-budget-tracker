5584c04f1bda3a86b12c07cf36ef7974a4bb7ff9
Brandon Armstrong <65680645+barmstrong1188@users.noreply.github.com>
2020-10-05 01:38:43 -0400

    finished off indexedDB

--- public/js/idb.js
+++ public/js/idb.js
@@ -10,7 +10,7 @@ request.onupgradeneeded = ({ target }) => {
 
 request.onsuccess = ({ target }) => {
     db = target.result;
-//check if app in online
+//check if app is online
     if (navigator.onLine) {
         checkDatabase();
     }
@@ -19,3 +19,45 @@ request.onsuccess = ({ target }) => {
 request.onerror = function(event) {
     console.log("Ruh Roh!" + event.target.errorCode);
 };
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    store.add(record);
+}
+
+function checkDatabase() {
+    //open transaction on db
+    const transaction = db.transaction(["pending"], "readwrite");
+    //access your object store
+    const store = transaction.objectStore("pending");
+    //get all records from store and set to a variable
+    const getAll = store.getAll();
+
+//upon a successfull .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+        fetch("api/transaction/bulk", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+            }
+        })
+        .then(response => {
+            return response.json();
+        })
+        .then(() => {
+            //delete records if successful
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+        });
+     }
+  };
+}
+//listen for app coming back online
+window.addEventListener("online", checkDatabase);
